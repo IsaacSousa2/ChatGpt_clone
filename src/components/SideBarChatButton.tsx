c2885e562b1f6f2fb84cb45cd141ca3cde63ca91
Rivelino Teixeira <132664436+RivelinoTeixeira@users.noreply.github.com>
2024-05-29 14:20:29 -0300

    Manutenção final de bugs (amem)

--- src/components/SideBarChatButton.tsx
+++ src/components/SideBarChatButton.tsx
@@ -46,67 +46,69 @@ export const SideBarChatButton = ({chatItem, active, onClick, onDelete, onEdit} 
 
     return(
 
-        <div onClick={handleClickButton} className={`flex items-center rounded-md p-3 text-sm cursor-pointer hover:bg-gray-500/10 ${active? 'bg-gray-500/20' : 'bg-transparent'}`}>
-            {/*onClick={handleClickButton} causa bugs*/}
+        <div>
+            <div className={`flex items-center rounded-md p-3 text-sm cursor-pointer hover:bg-gray-500/10 ${active? 'bg-gray-500/20' : 'bg-transparent'}`}>
+                {/*onClick={handleClickButton} causa bugs*/}
 
-            <div className="mr-3 text-white">
-                
-                {!deleting && <CiChat1 width={16} height={16} /> }
-                {deleting && <FaTrash width={16} height={16} /> }
-
-            </div>
-
-            <div  className="flex-1 text-sm overflow-x-hidden">
-
-            {editing &&  
-                <input 
-                    className="text-white bg-transparent w-full text-sm outline-none border border-blue-500"
-                    value={titleInput}
-                    onChange={e => setTitleInput(e.target.value)}
-                />
-            }
-            {!editing &&  
-                <div className="border border-transparent truncate">
-
-                    {!deleting && chatItem.title}
-                    {deleting && `Delete "${chatItem.title}"`}
+                <div className="mr-3 text-white">
+                    
+                    {!deleting && <CiChat1 width={16} height={16} /> }
+                    {deleting && <FaTrash width={16} height={16} /> }
 
                 </div>
-            }
+
+                <div className="flex-1 text-sm overflow-x-hidden">
+
+                {editing &&  
+                    <input 
+                        className="text-white bg-transparent w-full text-sm outline-none border border-blue-500"
+                        value={titleInput}
+                        onChange={e => setTitleInput(e.target.value)}
+                    />
+                }
+                {!editing  &&  
+                    <div onClick={handleClickButton} className="border border-transparent truncate">
+
+                        {!deleting && chatItem.title}
+                        {deleting && `Delete "${chatItem.title}"`}
+
+                    </div>
+                }
+
+                </div>
+
+                {active && !deleting && !editing && 
+                <div className="flex">
+
+                        <div 
+                        onClick={() => setEditing(true)}
+                        className="cursor-pointer mx-1 opacity-60 hover:opacity-100">
+                            <FiEdit3 height={16} width={16} />
+                        </div>
+                        <div 
+                        onClick={() => setDeleting(true)}
+                        className="cursor-pointer mx-1 opacity-60 hover:opacity-100">
+                            <FaTrash width={16} height={16} />
+                        </div>
+
+                </div>
+                }
+
+                {(deleting || editing) && 
+                <div className="flex"> 
+                        <div 
+                        onClick={handleConfirmButton}
+                        className="cursor-pointer mx-1 opacity-60 hover:opacity-100">
+                            <FaCheck height={16} width={16} />
+                        </div>
+                        <div 
+                        onClick={handleCancelButton}
+                        className="cursor-pointer mx-1 opacity-60 hover:opacity-100">
+                            <IoMdClose width={16} height={16} />
+                        </div>
+                </div>}
 
             </div>
-
-            {active && !deleting && !editing && 
-            <div className="flex">
-
-                    <div 
-                    onClick={() => setEditing(true)}
-                    className="cursor-pointer mx-1 opacity-60 hover:opacity-100">
-                        <FiEdit3 height={16} width={16} />
-                    </div>
-                    <div 
-                    onClick={() => setDeleting(true)}
-                    className="cursor-pointer mx-1 opacity-60 hover:opacity-100">
-                        <FaTrash width={16} height={16} />
-                    </div>
-
-            </div>
-            }
-
-            {(deleting || editing) && 
-            <div className="flex"> 
-                    <div 
-                    onClick={handleConfirmButton}
-                    className="cursor-pointer mx-1 opacity-60 hover:opacity-100">
-                        <FaCheck height={16} width={16} />
-                    </div>
-                    <div 
-                    onClick={handleCancelButton}
-                    className="cursor-pointer mx-1 opacity-60 hover:opacity-100">
-                        <IoMdClose width={16} height={16} />
-                    </div>
-            </div>}
-
         </div>
 
     )
